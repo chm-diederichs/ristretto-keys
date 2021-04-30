5f83d9cc8f98131a0245fa089e22582d8fc5e315
Christophe Diederichs <chris.d.itunes@gmail.com>
2021-04-30 14:01:14 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const sodium = require('sodium-native')
+const assert = require('nanoassert')
+
+module.exports = class Key {
+  constructor (opts = {}) {
+    this.sk = opts.sk || randomScalar(opts.seed)
+    this.pk = new Uint8Array(sodium.crypto_core_ristretto255_BYTES)
+
+    sodium.crypto_scalarmult_ristretto255_base(this.pk, this.sk)
+    assert(sodium.crypto_core_ristretto255_is_valid_point, 'invalid ristretto key')
+  }
+
+  dh (pk) {
+    assert(pk.byteLength === sodium.crypto_core_ristretto255_BYTES)
+
+    const output = Buffer.alloc(sodium.crypto_scalarmult_ristretto255_BYTES)
+
+    sodium.crypto_scalarmult_ristretto255(
+      output,
+      this.sk,
+      pk
+    )
+
+    return output
+  }
+
+  tweak (scalar) {
+    assert(scalar.byteLength === sodium.crypto_core_ristretto255_SCALARBYTES)
+
+    const output = Buffer.alloc(sodium.crypto_scalarmult_ristretto255_BYTES)
+
+    sodium.crypto_core_ristretto255_scalar_add(
+      output,
+      this.sk,
+      scalar
+    )
+
+    return new Key({ sk: output })
+  }
+}
+
+function randomScalar () {
+  const s = new Uint8Array(sodium.crypto_core_ristretto255_SCALARBYTES)
+  sodium.crypto_core_ristretto255_scalar_random(s)
+  return s
+}
---
+++ test.js
@@ -0,0 +1,23 @@
+const Key = require('./')
+const sodium = require('sodium-native')
+
+const scalar = Buffer.alloc(sodium.crypto_core_ristretto255_SCALARBYTES)
+const check1 = Buffer.alloc(sodium.crypto_core_ristretto255_BYTES)
+const check2 = Buffer.alloc(sodium.crypto_core_ristretto255_BYTES)
+
+const trials = 1000
+let fail = 0
+
+for (let i = 0; i < trials; i++) {
+  sodium.randombytes_buf(scalar)
+  const a = new Key()
+  const b = a.tweak(scalar)
+  sodium.crypto_scalarmult_ristretto255_base(check1, scalar)
+
+  if (sodium.crypto_core_ristretto255_add(check2, a.pk, check1) !== 0) {
+    sodium.crypto_core_ristretto255_sub(check2, a.pk, check1)
+  }
+  if (!Buffer.compare(check2, Buffer.from(b.pk))) fail++
+}
+
+console.log('fails', Math.round(fail * 100 / trials) + '%')
